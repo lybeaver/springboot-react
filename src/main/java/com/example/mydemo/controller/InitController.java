414ff63734002ced7e824b717f85310b751331c5
chenglin <LvLin22@126.com>
2018-09-28 18:06:55 +0800

    git

--- src/main/java/com/example/mydemo/controller/InitController.java
+++ src/main/java/com/example/mydemo/controller/InitController.java
@@ -1,15 +1,19 @@
 package com.example.mydemo.controller;
 
+import com.example.mydemo.beans.tUser;
+import com.example.mydemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.xml.ws.RequestWrapper;
+import java.util.List;
 
 @Controller
-@RequestMapping("/scripts/build")
+@RequestMapping("/public")
 public class InitController {
+    @Autowired
+    private UserService userService;
     @RequestMapping("/index")
     public String init() {
         return "/index";
@@ -17,5 +21,11 @@ public class InitController {
     @RequestMapping(value = "/login")
     public String login() {
         return "/login";
+    }
+    @RequestMapping(value = "/getUser")
+    @ResponseBody
+    public List<tUser> getUser(@RequestBody tUser user) {
+
+        return userService.getUsers(user.getLoginName(),user.getPassword());
     }
 }
--- src/main/resources/scripts/src/App.js
+++ src/main/resources/scripts/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-            <a href={"/scripts/build/login"}>turn</a>
+            <a href={"/public/login"}>turn</a>
         </p>
       </div>
     );
