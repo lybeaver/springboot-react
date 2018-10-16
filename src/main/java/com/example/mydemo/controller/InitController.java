09a7412cbe4fde1148ef641387191e1f9aee9651
ChengLin <LvLin22@126.com>
2018-10-17 00:02:24 +0800

    try

--- src/main/java/com/example/mydemo/controller/InitController.java
+++ src/main/java/com/example/mydemo/controller/InitController.java
@@ -1,5 +1,6 @@
 package com.example.mydemo.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.example.mydemo.beans.tUser;
 import com.example.mydemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,25 +8,37 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.xml.ws.RequestWrapper;
+import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/public")
 public class InitController {
     @Autowired
     private UserService userService;
-    @RequestMapping("/index")
-    public String init() {
-        return "/index";
+    @RequestMapping(method=RequestMethod.GET,value="/index")
+    public List<tUser> init() {
+        List<tUser> userList = new ArrayList<tUser>();
+        tUser user1 = new tUser();
+        tUser user2 = new tUser();
+        user1.setName("chenglin");
+        user1.setPassword("123456");
+        user2.setName("tom");
+        user2.setPassword("123456");
+        userList.add(user1);
+        userList.add(user2);
+        return userList;
+    }
+    @RequestMapping(method=RequestMethod.GET,value="/getUser")
+    public tUser getUser() {
+        tUser user1 = new tUser();
+        user1.setName("chenglin");
+        user1.setPassword("123456");
+        return user1;
     }
     @RequestMapping(value = "/login")
     public String login() {
         return "/login";
     }
-    @RequestMapping(value = "/getUser")
-    @ResponseBody
-    public List<tUser> getUser(@RequestBody tUser user) {
 
-        return userService.getUsers(user.getLoginName(),user.getPassword());
-    }
 }
--- src/main/resources/scripts/src/login.js
+++ src/main/resources/scripts/src/login.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React from 'react'
+import axios from 'axios'
 class Header extends React.Component{
+    state = {
+        loginState : false
+    }
+    checkLogin() {
+        axios.get("http://localhost:8080/public/index")
+            .then(response=>{
+                console.log(response.data)
+            })
+    }
+    // componentDidMount() {
+    //     console.log("login success")
+    // }
     render(){
+
         const loginId = "loginForm";
         return(<form className="form-signin" id={loginId} method={'submit'}>
             <h2 className="form-signin-heading"> 请登录 </h2>
             <label htmlFor="inputEmail" className="sr-only"> 邮箱地址
             </label>
-            <input type="email" id="inputEmail" className="form-control" placeholder="邮箱地址" required
-                   autoFocus/>
+            <input type="email" id="inputEmail" className="form-control" placeholder="邮箱地址" ref={input => this.input = input}/>
             <label htmlFor="inputPassword" className="sr-only"> 密  码</label>
             <input type="password" id="inputPassword" className="form-control" placeholder="密码" required/>
-            <button className="btn btn-lg btn-primary btn-block" type="button"> 登 录
+            <button className="btn btn-lg btn-primary btn-block" type="button" onClick={this.checkLogin}> 登 录
             </button>
         </form>);
     }
