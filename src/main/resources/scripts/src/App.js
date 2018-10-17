d74d6f7748a2fa496f37458851bff432b30ca640
chenglin <LvLin22@126.com>
2018-10-17 18:02:16 +0800

    git

--- src/main/resources/scripts/src/App.js
+++ src/main/resources/scripts/src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <Router> <div>
+              <ul>
+              <li><Link to='/'>首页</Link></li>
+              <li><Link to='/about'>关于</Link></li>
+              <li><Link to='/topics'>主题列表</Link></li>
+              </ul> <hr/>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} /> 
+              <Route path="/topics" component={Topics} />
+          </div> </Router>
       </div>
     );
   }
--- src/main/resources/scripts/src/index.js
+++ src/main/resources/scripts/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Login from './login';
-ReactDOM.render(<Login />,document.getElementById('loginForm'));
+import './index.css'
+import NormalLoginForm from './components/Login'
+
+ReactDOM.render(<NormalLoginForm />, document.getElementById('loginForm'));
